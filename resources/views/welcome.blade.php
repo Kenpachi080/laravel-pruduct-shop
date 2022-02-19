4f7009e91efcd76832ee390f26bb64521a37e998
Kenpachi080 <saxah23332@gmail.com>
2022-02-19 17:54:40 +0600

    main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,13 @@
 @extends('constructors.app')
 @section('title')
     <title>Welcome</title>
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
 @endsection
 
 @section("content")
     <h4>privet</h4>
+
+    <section class="stap1">
+        <h1>privet</h1>
+    </section>
 @endsection
