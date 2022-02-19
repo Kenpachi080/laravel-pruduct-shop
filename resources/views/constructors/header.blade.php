fe730b05d01eb2d37c1a15de91acaead440ce8c3
Kenpachi080 <saxah23332@gmail.com>
2022-02-19 21:06:35 +0600

    main

---
+++ resources/views/constructors/header.blade.php
@@ -0,0 +1,19 @@
+<header class="header_navigation">
+    <div class="container">
+        <nav>
+            <li><a href="{{ route('home') }}">О салоне</a></li>
+            <li><a href="{{ route('home') }}">Магазин</a></li>
+            <li><a href="{{ route('home') }}">Преимущества</a></li>
+            <li><a href="{{ route('home') }}">Контакты</a></li>
+        </nav>
+        <div class="contacts">
+            <div class="phone">
+                <p>+7 707 842 0012</p>
+            </div>
+            <div class="contact">
+                <a href="/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                <a href="/"><i class="fa fa-instagram"></i></a>
+            </div>
+        </div>
+    </div>
+</header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,18 @@
 @extends('constructors.app')
 @section('title')
-    <title>Welcome</title>
-    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
+<title>Welcome</title>
+<link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
 @endsection
 
 @section("content")
-    <h4>privet</h4>
-
-    <section class="stap1">
-        <h1>privet</h1>
-    </section>
+<section style="height:100vh;" class="stap1">
+    <div class="container">
+        <div class="content_container">
+        <h1>TASHKENT<br>FRUITS</h1>
+        <p>Оптом и в розницу</p>
+        <p>Доставка по всему РК</p>
+        <p>Более 200 наименовании продукта</p>
+    </div>
+</div>
+</section>
 @endsection
